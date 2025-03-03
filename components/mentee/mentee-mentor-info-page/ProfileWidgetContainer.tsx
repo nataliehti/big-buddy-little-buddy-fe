6b814a9a18482bc0347f7799a0f4acc99584e09c
nataliehti <146873462+nataliehti@users.noreply.github.com>
2025-03-02 16:05:52 -0800

    updated mentor info page

--- components/mentee/mentee-mentor-info-page/ProfileWidgetContainer.tsx
+++ components/mentee/mentee-mentor-info-page/ProfileWidgetContainer.tsx
@@ -1,5 +1,9 @@
 import { Avatar, Button, Card, Group, Text, Badge } from '@mantine/core';
 import classes from './ProfileWidgetContainer.module.css';
+import {
+  IconBook,
+
+} from '@tabler/icons-react';
 
 const stats = [
   { value: '7', label: 'YOE' }, // years of experience
@@ -16,13 +20,13 @@ function ProfileSummary() {
   return (
     <Card className={classes.profileCard}>
           <Avatar
-            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-9.png"
+            src="https://media.istockphoto.com/id/1150742429/photo/asian-girl-taking-a-selfie-in-rain.jpg?s=612x612&w=0&k=20&c=SQITcq6XT-JTScQLFkTtvvj6yOpP04EPRUOq5Y-3-Hg="
             radius={0}
             className={classes.avatar}
           />
           <div className={classes.profileDetails}>
             <Text fz="lg" fw={620} mt="sm" className={classes.name}>
-                Bill Headbanger
+              Serena Nguyen
             </Text>
             <Text fz="md" c="dimmed" className={classes.role}>
               Fullstack engineer
@@ -60,27 +64,54 @@ function Biography(){
 
   return(
     <Card className={classes.bioCard}>
-      <Text fz="lg" fw={620} mt="sm" className={classes.about}> About Me </Text>
+      <Text fz="md" fw={620} mt="sm" className={classes.about}> About Me </Text>
       <Text fz="md" mt="sm" className={classes.selfIntro}>
           Engineering is my passion, and I enjoy mentoring young engineers to achieve their career goals.
           </Text>
 
-      <Text fz="md" fw={500} mt="lg" className={classes.about}> Favorite Quote </Text>
+      <Text fz="md" fw={620} mt="sm" className={classes.about}> Favorite Quote </Text>
       <Text fz="md" mt="sm" className={classes.selfIntro}>
-          The important thing is not to stop questioning. Curiosity has its own reason for existing.
+          "The important thing is not to stop questioning. Curiosity has its own reason for existing."
           </Text>
       
-      <Text fz="md" fw={500} mt="lg" className={classes.about}> Hobbies </Text>
-      <Group mt="md" gap={15} mb="md" className={classes.tags}>
+      <Text fz="md" fw={620} mt="sm" className={classes.about}> Hobbies </Text>
+      <Group mt="md" gap={15} mb="xs" className={classes.tags}>
           {items}
         </Group>
-        <Text fz="md" fw={500} mt="lg" className={classes.about}> Favorite Book </Text>
+        <Text fz="md" fw={620} mt="sm" mb="xs" className={classes.about}> Favorite Book </Text>
+        <div className={classes.books}>
+          <Card className={classes.smallCard}>
+            <img
+              src="https://m.media-amazon.com/images/I/910vYI-gm0L._AC_UF1000,1000_QL80_.jpg"
+              className={classes.img}
+            />
+              <div className={classes.group}>
+                <Text fw={520}>The Mountains Sing</Text>
+                <Text fz="xs" fw={300}>By Nguyễn Phan Quế Mai</Text>
+              </div>
+            </Card>
+            <Card className={classes.smallCard}>
+            <img
+              src="https://m.media-amazon.com/images/I/61eh6F9MG7L._AC_UF1000,1000_QL80_.jpg"
+              className={classes.img}
+            />
+              <div className={classes.group}>
+                <Text fw={520}>The Best We Could Do</Text>
+                <Text fz="xs" fw={300}>By Thi Bui</Text>
+              </div>
+            </Card>
+        </div>
+          <Text fz="md" fw={620} mt="sm" className={classes.about}> Favorite Movie </Text>
       <Text fz="md" mt="sm" className={classes.selfIntro}>
-          book
-          </Text>
-          <Text fz="md" fw={500} mt="lg" className={classes.about}> Favorite Movie </Text>
-      <Text fz="md" mt="sm" className={classes.selfIntro}>
-          movie
+      <Card className={classes.smallCard}>
+      <img
+              src="https://m.media-amazon.com/images/I/71WSzS6zvCL._AC_UF1000,1000_QL80_.jpg"
+              className={classes.img}
+            />
+              <div className={classes.group}>
+                <Text fw={520}>The Hunger Games</Text>
+              </div>
+            </Card>
           </Text>
       
     
